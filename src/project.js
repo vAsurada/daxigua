a342c9a3e2128eadc4dd97a586256876c8baec22
vAsurada <79357779+vAsurada@users.noreply.github.com>
2021-02-21 19:55:59 +0900

    Update project.js

--- src/project.js
+++ src/project.js
@@ -6,7 +6,7 @@
 if (selectModal) {
   let extraScoreStr = '';
   while (isNaN(parseInt(extraScoreStr))) {
-    extraScoreStr = prompt('몽준이의 부산대 편입을 ', '1').trim();
+    extraScoreStr = prompt(' ', '1').trim();
   }
   extraScore = parseInt(extraScoreStr)
 }
